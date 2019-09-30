eab7ad1080dac9bb2c10de2dcab83c1f7ccdb5ac
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-30 11:36:47 -0700

    setup

---
+++ challenge_1/client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+class App extends React.Component{
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+export default App;
+
+// import React from 'react';
+
+// const App = () => {
+//   return (
+//     <div>
+//       Hello
+//     </div>
+//   );
+// }
+
+// export default App;
