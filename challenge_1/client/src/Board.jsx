3957ed177416670e775fde2ce4ee155a386cb802
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-30 15:49:18 -0700

    adding draw and win helper functions, fixing square so it cant be clicked more than once, updating message with game status/current player

--- challenge_1/client/src/Board.jsx
+++ challenge_1/client/src/Board.jsx
@@ -10,15 +10,24 @@ class Board extends React.Component{
         [null, null, null],
         [null, null, null]
       ],
-      currentPlayerIsX: true
+      currentPlayerIsX: true,
+      gameStatus: null
     };
     this.handleClick = this.handleClick.bind(this)
   }
 
   render() {
+    var message;
+    if (this.state.gameStatus === null) {
+      message = `Current player: ${(this.state.currentPlayerIsX) ? 'X' : 'O'}`;
+    } else if (this.state.gameStatus === 'draw') {
+      message = 'Game status: draw';
+    } else {
+      message = `Game status: winner is ${(this.state.currentPlayerIsX) ? 'X' : 'O'}`;
+    }
     return (
       <div>
-        <div>Current player: {(this.state.currentPlayerIsX) ? 'X' : 'O'}</div>
+        <div>{message}</div>
         <div>{this.state.arrangement.map((row, index) => <Row key={index} values={row} row={index} onclick={this.handleClick} /> )}</div>
         <div><button onClick={() => {this.resetBoard()}}>Reset board</button></div>
       </div>
@@ -26,21 +35,36 @@ class Board extends React.Component{
   }
 
   handleClick(row, column) {
-    var newArrangement = this.getNewArrangement(row, column);
-    this.setState({
-      currentPlayerIsX: !this.state.currentPlayerIsX,
-      arrangement: newArrangement
-    })
+    if (this.state.gameStatus === null) { // game still in progress
+      var newArrangement = this.getNewArrangement(row, column);
+      var newGameStatus = null;
+      var newCurrentPlayer = this.state.currentPlayerIsX;
+      // update newGameStatus if there is a win or draw
+      if (this.checkForWin(newArrangement)) {
+        newGameStatus = 'win';
+      } else if (this.checkForDraw(newArrangement)) {
+        newGameStatus = 'draw';
+      }
+      if (newGameStatus === null) {
+        var newCurrentPlayer = !this.state.currentPlayerIsX;
+      }
+      this.setState({
+        currentPlayerIsX: newCurrentPlayer,
+        arrangement: newArrangement,
+        gameStatus: newGameStatus
+      });
+    }
   }
 
   resetBoard() {
     this.setState({
-      currentPlayerIsX: true,
       arrangement: [
         [null, null, null],
         [null, null, null],
         [null, null, null]
-      ]
+      ],
+      currentPlayerIsX: true,
+      gameStatus: null
     })
   }
 
@@ -51,8 +75,47 @@ class Board extends React.Component{
     return newArrangement;
   }
 
-  checkForWinner() {
-    this.state.arrangement;
+  checkForWin(arrangement) {
+    var winningCombos = [
+      // rows
+      [[0,0], [0,1], [0,2]],
+      [[1,0], [1,1], [1,2]],
+      [[2,0], [2,1], [2,2]],
+      // columns
+      [[0,0], [1,0], [2,0]],
+      [[0,1], [1,1], [2,1]],
+      [[0,2], [1,2], [2,2]],
+      // diagonals
+      [[0,0], [1,1], [2,2]],
+      [[2,0], [1,1], [0,2]]
+    ];
+    for (var i = 0; i < winningCombos.length; i++) {
+        var winningCombo = winningCombos[i];
+        var count = 0;
+        for (var j = 0; j < 3; j++) {
+            var winningComboRow = winningCombo[j][0];
+            var winningComboColumn = winningCombo[j][1];
+            var player = (this.state.currentPlayerIsX) ? 'X' : 'O';
+            if (arrangement[winningComboRow][winningComboColumn] === player) {
+                count++;
+            }
+        }
+        if (count === 3) {
+            return true;
+        }
+    }
+    return false;
+  }
+
+  checkForDraw(arrangement) {
+    for (var row = 0; row < arrangement.length; row++) {
+      for (var column = 0; column < 3; column++) {
+          if (arrangement[row][column] === null) {
+            return false;
+          }
+      }
+    }
+    return true;
   }
 
 }
--- challenge_1/client/src/Square.jsx
+++ challenge_1/client/src/Square.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 function Square ({value, row, column, onclick}) {
-  return (
-      <button onClick={() => {onclick(row, column)}}>
+  if (value === null) {
+    return (
+        <button onClick={() => {onclick(row, column)}}>
+          {value}
+        </button>
+    );
+  } else { // square has already been filled, can't be clicked again
+    return (
+      <button>
         {value}
       </button>
-  );
+    );
+  }
 }
 
 export default Square;
