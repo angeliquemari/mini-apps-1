24ab1a3987d5c6ae8f9029916cd3e7f11ffc1278
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-01 12:17:28 -0700

    remove ; from end of json sample file, update index.html to use file picker, update server.js to use multer to read file in post req

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,19 +2,22 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
+const morgan = require('morgan');
+const multer  = require('multer')
+const upload = multer();
 
 app.use(express.static('client'));
-app.use(express.urlencoded())
+app.use(morgan('dev'));
+// app.use(express.urlencoded());
 // app.use(express.json());
 
 app.get('/', (req, res) => {
   res.end();
 });
 
-app.post('/', function (req, res) {
-  console.log('req json: ', req.body.data);
-  var csv = getCsv(JSON.parse(req.body.data));
-  console.log('res csv: ', csv);
+app.post('/', upload.single('data'), function (req, res) {
+  var fileText = req.file.buffer.toString();
+  var csv = getCsv(JSON.parse(fileText));
   res.type('text/csv').send(csv);
 })
 
@@ -24,24 +27,24 @@ var getCsv = function(parsedData) {
   var columns = [];
   var keys = Object.keys(parsedData);
   for (var key in keys) {
-      if (keys[key] !== 'children') {
-          columns.push(keys[key]);
-      }
+    if (keys[key] !== 'children') {
+      columns.push(keys[key]);
+    }
   }
   var csvStr = columns.join(',') + '\n';
   var helper = function(parsedData) {
-      for (var i = 0; i < columns.length; i++) {
-          csvStr += parsedData[columns[i]] || '';
-          if (i === columns.length - 1) {
-              csvStr += '\n';
-          } else {
-              csvStr += ',';
-          }
+    for (var i = 0; i < columns.length; i++) {
+      csvStr += parsedData[columns[i]] || '';
+      if (i === columns.length - 1) {
+        csvStr += '\n';
+      } else {
+        csvStr += ',';
       }
-      var children = parsedData['children'];
-      for (var j = 0; j < children.length; j++) {
-          helper(children[j]);
-      }
+    }
+    var children = parsedData['children'];
+    for (var j = 0; j < children.length; j++) {
+      helper(children[j]);
+    }
   }
   helper(parsedData);
   return csvStr;
