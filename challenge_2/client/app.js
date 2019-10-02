637e45913171f22b20a7e7b179122a0f727a7ca8
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-01 19:35:13 -0700

    update commented out code in app.js

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,20 +1,34 @@
-// $( document ).ready(function() {
-//   $('#json-submission').submit(function(e) {
-//     e.preventDefault();
-//     var form = $('.file')[0];
-//     var file = form.files[0];
-//     var formData = new FormData();
-//     formData.append('file', file);
-//     $.ajax({
-//       type: 'POST',
-//       data: formData,
-//       url: '/',
-//       cache: false,
-//       contentType: false,
-//       processData: false,
-//       success: (data) => {
-//         $('#csv-report').empty().append(data);
-//       }
-//     });
+// $('#json-submission').submit(function(e) {
+//   e.preventDefault();
+//   var form = $('.file')[0];
+//   var file = form.files[0];
+//   var formData = new FormData();
+//   formData.append('file', file);
+//   $.ajax({
+//     type: 'POST',
+//     data: formData,
+//     url: '/',
+//     cache: false,
+//     contentType: false,
+//     processData: false,
+//     success: (data) => {
+//       $('#csv-report').empty().append(data);
+//       $('#download').show();
+//     }
+//   });
+// });
+
+// $('#download').click(function(e) {
+//   e.preventDefault();
+//   $.ajax({
+//     type: 'GET',
+//     url: '/download',
+//     success: (data) => {
+//       var blob = new Blob([data]);
+//       var link = document.createElement('a');
+//       link.href = window.URL.createObjectURL(blob);
+//       link.download = "convertedJSON.csv";
+//       link.click();
+//     }
 //   });
 // });
