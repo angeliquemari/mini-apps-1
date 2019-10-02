00e487f386fd084f651e638793066b7687b7054c
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-02 13:59:14 -0700

    webpack and dependencies installation, setup server, placeholder react component rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+function Test(props) {
+  return (
+    <div>Testing</div>
+  )
+}
 
+ReactDOM.render(<Test />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
 
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
+app.get('/', (req, res) => {
+  res.end();
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
