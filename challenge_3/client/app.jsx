c4f88638dc98f9e2d7b4f011c310eb4ff68eaf6d
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-02 16:04:13 -0700

    setup db and tables, placeholder client get request creates record in purchases table

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const axios = require('axios');
 
-function Test(props) {
-  return (
-    <div>Testing</div>
-  )
-}
+class Checkout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stage: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-ReactDOM.render(<Test />, document.getElementById('app'));
+  render () {
+    return (
+    <div>
+      <button id="checkout-button" onClick={this.handleClick}>Checkout</button>
+    </div>
+    )
+  }
+
+  handleClick(e) {
+    axios.get('/newpurchase')
+    .then((res) => {
+      console.log('get request successful:', res);
+    })
+    .catch((err) => {
+      console.log('get request failed:', err);
+    });
+
+  }
+  // if checkout i.e. step 0
+  // create new record in purchases table
+  // set this.state.purchaseId
+
+  // if form 1 i.e. step 1
+  // create new record in users table
+  // update record in purchases table
+  // set this.state.userId
+
+  // if form 2 i.e. step 2
+  // create new record in addresses table
+  // update record in purchases table
+
+  // if form 3 i.e. step 3
+  // create new record in payments table
+  // update record in purchases table
+  // reset to index page
+
+  // outstanding Qs - separate click handlers and forms for each stage?
+};
+
+ReactDOM.render(<Checkout />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,12 +2,115 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const morgan = require('morgan');
+const Sequelize = require('sequelize');
 
+// set up logging and serving client file
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.end();
+// set up db connection and tables
+const db = new Sequelize('checkout', 'root', 'chatterbox', {dialect: 'mysql'});
+const User = db.define('users', {
+  name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  }
 });
+const Address = db.define('addresses', {
+  userId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  line1: {
+    type: Sequelize.STRING
+  },
+  line2: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  city: {
+    type: Sequelize.STRING
+  },
+  state: {
+    type: Sequelize.STRING
+  },
+  zip: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.STRING
+  }
+});
+const Payment = db.define('payments', {
+  userId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  creditCardNumber: {
+    type: Sequelize.INTEGER
+  },
+  expiryDate: {
+    type: Sequelize.DATE
+  },
+  CVV: {
+    type: Sequelize.STRING
+  },
+  billingZip: {
+    type: Sequelize.STRING
+  }
+});
+const Purchase = db.define('purchases', {
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  addressId: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    references: {
+      model: Address,
+      key: 'id'
+    }
+  },
+  paymentId: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    references: {
+      model: Payment,
+      key: 'id'
+    }
+  }
+});
+db.sync();
 
+// route handling
+app.get('/newpurchase', (req, res) => {
+  console.log('received get request to /newpurchase');
+  Purchase.create({userId: null})
+  .then((purchase) => {
+    console.log('new purchase id: ', purchase.id);
+    res.end();
+  })
+  .catch(err => {
+    console.error('failed to create purchase', err);
+    res.end();
+  });
+})
+
+// listen for client requests
 app.listen(port, () => console.log(`Listening on port ${port}`));
