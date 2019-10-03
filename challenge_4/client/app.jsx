7cd8bcbf2f72b383654fec866996e767d604c733
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-03 13:06:36 -0700

    render initial grid, pass along click handler that gets invoked with index of column clicked

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -2,10 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const axios = require('axios');
 
-function Test(props) {
+function Row(props) {
   return (
-    <div>Test</div>
+    <tr>
+      {props.row.map((cell, colIndex) => <td key={colIndex} onClick={() => {props.onclick(colIndex)}}>{cell}</td>)}
+    </tr>
   );
 }
 
-ReactDOM.render(<Test />, document.getElementById('app'));
+function Grid(props) {
+  return props.grid.map((row, rowIndex) => <Row key={rowIndex} row={row} onclick={props.onclick} /> );
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null]
+      ]
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  render() {
+    return (
+      <table border="1">
+        <tbody>
+        <Grid onclick={this.handleClick} grid={this.state.grid}/>
+        </tbody>
+      </table>
+    );
+  }
+  handleClick(colIndex) {
+    alert('colIndex = ' + colIndex);
+
+
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById('app'));
