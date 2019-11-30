3f3328d7ced98043ebfb79647c8176aff79bcf4b
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-30 13:24:56 -0800

    tests

--- challenge_4/spec.js
+++ challenge_4/spec.js
@@ -1,10 +1,26 @@
-// attempt at tests failed, lost draft of them after a reset to origin
 var assert = require('assert');
+// var helpers = require('./client/checkers.js');
+import {checkForTie, checkForWin} from './client/checkers.js';
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when value is not present', function() {
-      assert.equal([1,2].indexOf(3), -1);
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when value is not present', function() {
+//       assert.equal([1,2].indexOf(3), -1);
+//     });
+//   });
+// });
+
+describe('Checkers', function() {
+  describe('#checkForTie()', function() {
+    it('should false for an empty grid', function() {
+      assert.equal(checkForTie([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null]
+      ]), false);
     });
   });
 });
